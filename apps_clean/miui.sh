0ecf399e10dce02faf9101cfcba84042dbbf29f4
Xayah <1249475336@qq.com>
2021-06-05 01:13:27 +0800

    perf: 修改MIUI精简名单

--- apps_clean/miui.sh
+++ apps_clean/miui.sh
@@ -3,4 +3,24 @@
 LOCALDIR=`cd "$( dirname $0 )" && pwd`
 cd $LOCALDIR
 
+systemdir=$1
+
+apps_dir="
+CameraTools
+Mipay
+MiuiBugReport
+Browser
+Calendar
+MiuiCamera
+MiuiGallery
+MiuiVideo
+Music
+QuickSearchBox
+talkback
+"
+
+for delete_dir in $apps_dir ;do
+  find $systemdir -type d -name "$delete_dir" | xargs rm -rf
+done
+
 rm -rf $1/data-app/*
